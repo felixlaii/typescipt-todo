e21d39a4ae0d4bde9c38adc633d564a3d9918657
Felix Lai <felix.lai@hotmail.com>
2022-12-20 14:21:03 -0500

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { TodoListItem } from './TodoListItem';
 
+interface TodoListProps {
+    todos: Array<Todo>;
+    toggleComplete: ToggleComplete;
+    onRemoveTodo: RemoveTodo;
+    editTodo: EditTodo;
+  }
+
 export const TodoList = () => {
   return (
     <div>TodoList</div>
