7d2c8364995fff2ad6edbc61e6e51b918021f894
Felix Lai <felix.lai@hotmail.com>
2022-12-20 14:19:12 -0500

    Update TodoListItem.tsx

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -2,44 +2,57 @@ import React, { useState } from "react";
 import { Dropdown } from "./Dropdown";
 
 interface TodoListItemProps {
-    todo: Todo;
-    toggleComplete: ToggleComplete;
-    onRemoveTodo: RemoveTodo;
-    editTodo: EditTodo;
-  }
+  todo: Todo;
+  toggleComplete: ToggleComplete;
+  onRemoveTodo: RemoveTodo;
+  editTodo: EditTodo;
+}
 
-export const TodoListItem: React.FC<TodoListItemProps> = ({ todo, toggleComplete, onRemoveTodo, editTodo }) => {
-    const [isEditOn, setIsEditOn] = useState<boolean>(false);
-    const [inputText, setInputText] = useState<string>(todo.text);
+export const TodoListItem: React.FC<TodoListItemProps> = ({
+  todo,
+  toggleComplete,
+  onRemoveTodo,
+  editTodo,
+}) => {
+  const [isEditOn, setIsEditOn] = useState<boolean>(false);
+  const [inputText, setInputText] = useState<string>(todo.text);
 
-    const onDelete = () => {
-        onRemoveTodo(todo);
-      }
+  const onDelete = () => {
+    onRemoveTodo(todo);
+  };
 
-    const onEdit = () => {
-        console.log('edit');
-      }
-      
-    const onTodoUpdate = (e: any) => {
-        let text = e.target.value;
-        setInputText(text);
-        editTodo(text);
-      }
-    
-    const dropdownOptions: Array<Option> = [
-        {
-            value: "delete",
-            onClick: onDelete,
-            color: "red",
-        },
-        {
-            value: "edit",
-            onClick: onEdit,
-            color: "blue",
-        }
-    ]
+  const onEdit = () => {
+    console.log("edit");
+  };
+
+  const onTodoUpdate = (e: any) => {
+    let text = e.target.value;
+    setInputText(text);
+    editTodo(text);
+  };
+
+  const dropdownOptions: Array<Option> = [
+    {
+      value: "delete",
+      onClick: onDelete,
+      color: "red",
+    },
+    {
+      value: "edit",
+      onClick: onEdit,
+      color: "blue",
+    },
+  ];
 
   return (
-    <div>TodoListItem</div>
-  )
-}
+    <li className={todo.complete ? "todo-row completed" : "todo-row"}>
+      <label>
+        <input
+          type="checkbox"
+          onChange={() => toggleComplete(todo)}
+          checked={todo.complete}
+        />
+      </label>
+    </li>
+  );
+};
