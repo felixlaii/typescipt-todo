67326e8f041506bbd7e9d9cb05d1a04c1a7155cb
Felix Lai <felix.lai@hotmail.com>
2022-12-20 13:41:35 -0500

    Update Dropdown.tsx

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 interface DropdownItemProps {
-    icon?: string
-    options: Array<Option>
-  }
+  icon?: string;
+  options: Array<Option>;
+}
 
 export const Dropdown: React.FC<DropdownItemProps> = ({ icon, options }) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen);
-      }
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div>Dropdown</div>
-  )
-}
+    <div className="dropdown">
+      <div className="dropdown-title" onClick={toggleDropdown}>
+        <BsThreeDotsVertical />
+      </div>
+    </div>
+  );
+};
