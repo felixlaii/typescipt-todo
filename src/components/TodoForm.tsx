61662b48a7ff0666f77f38d64fca47e2d2ff1a04
Felix Lai <felix.lai@hotmail.com>
2022-12-20 13:56:20 -0500

    Update TodoForm.tsx

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,5 +1,9 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
 
+interface TodoFormProps {
+    addTodo: AddTodo;
+  }
+  
 export const TodoForm = () => {
   return (
     <div>TodoForm</div>
